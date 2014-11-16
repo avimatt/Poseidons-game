e37436969fcb6e5078c429453c468f3d7d165c27
Avi Mattenson <avimatt@umich.edu>
2014-11-16 14:41:42 -0500

    Added Lactation and Ships Classes

---
+++ proj4/src/eecs285/proj4/group/Ships/Destroyer.java
@@ -0,0 +1,58 @@
+package eecs285.proj4.group.Ships;
+
+public class Destroyer extends Ship
+{
+  private int health;
+
+  public Destroyer(){
+    super();
+    // health = TBD;
+  }
+  
+  @Override
+  public int getSpeed()
+  {
+    // Choose a value for speed and put it here
+    return 0;
+  }
+
+  @Override
+  public int getAttackPower()
+  {
+    // Choose a value for AttackPower and put it here
+    return 0;
+  }
+
+  @Override
+  public int getHealth()
+  {
+    return health;
+  }
+
+  @Override
+  public void setHealth(int healthIn){
+    health = healthIn;
+  }
+  
+  
+  // Radius from the center of the ship determined by the Location
+  @Override
+  public int getVisibilityRadius(){
+    // Choose a value for visibilityRadius and put it here
+    return 0;
+  }
+  
+//Radius from the center of the ship determined by the Location
+  @Override
+  public int getAttackRadius(){
+    // Choose a value for attackRadius and put it here
+    return 0;
+  }
+
+  @Override
+  public int getSize()
+  {
+    return 3;
+  }
+
+}
---
+++ proj4/src/eecs285/proj4/group/Ships/Ship.java
@@ -0,0 +1,41 @@
+package eecs285.proj4.group.Ships;
+
+import eecs285.proj4.group.Location;
+
+public abstract class Ship
+{
+  private static int nextID = 0;
+  private int id;
+  private Location currentLoc;
+  
+  public Ship(){
+    id = nextID++;    
+  }
+  
+  public int getID(){
+    return id;
+  }
+  
+  public Location getCurrentLocation(){
+    return currentLoc;
+  }
+  
+  public void setCurrentLoaction(){
+    
+  }
+  
+  public abstract int getSpeed();
+  
+  public abstract int getAttackPower();
+  
+  public abstract int getHealth();
+  
+  public abstract void setHealth(int healthIn);
+  
+  public abstract int getVisibilityRadius();
+  
+  public abstract int getAttackRadius();
+  
+  public abstract int getSize();
+  
+}
