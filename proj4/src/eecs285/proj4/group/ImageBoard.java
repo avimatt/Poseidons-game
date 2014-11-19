640f6b8faf108263f122fdc0ba36c216c9579975
Yossef Rosenstein <yossef.rosenstein@gmail.com>
2014-11-19 15:25:53 -0500

    Started structuring and ImageBoard class, will add functionality, once I learn more about buffered images.

---
+++ proj4/src/eecs285/proj4/group/ImageBoard.java
@@ -0,0 +1,33 @@
+package eecs285.proj4.group;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by yossier on 11/19/14.
+ */
+public class ImageBoard extends JPanel{
+  private BufferedImage boardImage;
+
+  public ImageBoard()
+  {
+    super();
+    boardImage = new BufferedImage(200,200,BufferedImage.TYPE_INT_RGB);
+  }
+
+  public void updateBoard()
+  {
+
+  }
+
+  public void drawBoat()
+  {
+
+  }
+
+  public void drawObject()
+  {
+
+  }
+
+}
