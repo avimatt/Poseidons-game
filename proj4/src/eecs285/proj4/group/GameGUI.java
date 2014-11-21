1f9770de9548699dd91e36a88fe51348555d4da2
Pietro Vitale <ptvitale@umich.edu>
2014-11-21 00:28:47 -0500

    Help Button works now

--- proj4/src/eecs285/proj4/group/GameGUI.java
+++ proj4/src/eecs285/proj4/group/GameGUI.java
@@ -2,6 +2,10 @@ package eecs285.proj4.group;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.*;
 
@@ -14,6 +18,8 @@ public class GameGUI extends JFrame
   JButton joinGameButton;
   JButton helpButton;
   JButton exitGameButton;
+  JButton okayButton;
+  JDialog helpFrame;
   GameListener gameListener;
 
   public GameGUI()
@@ -63,13 +69,81 @@ public class GameGUI extends JFrame
     {
       if (e.getSource() == startGameButton)
       {
+        remove(gameOptions);
+        setTitle("Poseidon's Game");
         setContentPane(new ImageBoard());
+        validate();
+      }
+      if(e.getSource() == joinGameButton)
+      {
+        
+      }
+      if(e.getSource() == helpButton)
+      {
+        getHelpframe();
       }
       if (e.getSource() == exitGameButton)
       {
         dispose();
       }
+      if(e.getSource() == okayButton)
+      {
+        helpFrame.dispose();
+      }
     }
+    
+    private void getHelpframe()
+    {
+       helpFrame = new JDialog();
+       helpFrame.setLayout(new BorderLayout());
+       JPanel buttons = new JPanel();
+       buttons.setLayout(new FlowLayout());
+       JTextArea helpCon = new JTextArea();
+       okayButton = new JButton("OK");
+       okayButton.addMouseListener(new GameListener());
+       helpFrame.setTitle("Help!");
+       helpFrame.setVisible(true);
+       helpFrame.setSize(600,400);
+       helpFrame.setResizable(false);
+       helpFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+       helpCon.setEditable(false);
+       helpCon.setWrapStyleWord(true);
+       helpCon.setLineWrap(true);
+       
+       
+       ////////READS IN HELP DOCUMENT////////////////////////////////////////
+       BufferedReader helpin;
+       try
+       {
+         helpin = new BufferedReader(new FileReader(getClass()
+             .getClassLoader().getResource("HelpDoc/HelpDocument.txt").getPath()));
+         String getline;
+         String saveline = "";
+         //gets the info. from the HelpDocument
+         saveline = new StringBuilder(helpin.readLine()).reverse().toString();
+         while((getline = helpin.readLine()) != null)
+         {
+           getline = new StringBuilder(getline).reverse().toString();
+           saveline = getline+"\n"+saveline;
+         }
+         helpin.close();
+         //reverses the string that comes in to display the help file correctly
+         helpCon.setText(new StringBuilder(saveline).reverse().toString());
+         helpCon.setCaretPosition(0);
+       }
+       catch( IOException e )
+       {
+        //should never happen
+        System.out.println("error!");
+       }
+       ///////////////////////////////////////////////////////////////////////////
+       JScrollPane scrollhelp = new JScrollPane(helpCon);
+       scrollhelp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+       helpFrame.add(scrollhelp, BorderLayout.CENTER);
+       buttons.add(okayButton);
+       helpFrame.add(buttons, BorderLayout.SOUTH);
+    }
+    
     public void mousePressed(MouseEvent e)
     {
 
